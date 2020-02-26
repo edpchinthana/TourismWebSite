f9bd9ed1cc2c70f45740790edf6604bcc66fa3a6
edpchinthana <edpchinthana@outlook.com>
2020-02-27 01:14:23 +0530

    SlideShow is done

--- js/myJS.js
+++ js/myJS.js
@@ -10,3 +10,40 @@ function scrollAppear(){
 }
 
 window.addEventListener('scroll',scrollAppear);
+
+//SlideShow
+var index = 1;
+hideAll();
+previewSlides(index);
+
+
+
+function nextSlide(n){
+    previewSlides(index+=n);
+}
+
+function prevSlide(n){
+    n=index-1;
+    previewSlides(n);
+}
+function currentSlide(n) {
+    previewSlides(n);
+  }
+
+function previewSlides(n){
+    hideAll();
+    if(n>4){
+        n=1;
+    }else if(n<1){
+        n=4;
+    }
+    index=n;
+    document.getElementById("slide"+n).setAttribute("style","display:block;");
+}
+
+function hideAll(){
+    var i;
+    for(i=1;i<5;i++){
+        document.getElementById("slide"+i).setAttribute("style","display:none;");
+    }
+}
