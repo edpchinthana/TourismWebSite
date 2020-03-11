1b3546e70981c5f68d7c8f8d054f4270cbdd2fb8
edpchinthana <edpchinthana@outlook.com>
2020-03-11 16:08:07 +0530

    places are added to explore

---
+++ js/myJS3.js
@@ -0,0 +1,56 @@
+//SlideShow
+var index = 1;
+hideAll();
+previewSlides(index);
+
+
+
+function nextSlide(n){
+    previewSlides(index+=n);
+}
+
+function prevSlide(n){
+    n=index-1;
+    previewSlides(n);
+}
+function currentSlide(n) {
+    previewSlides(n);
+  }
+
+function previewSlides(n){
+    hideAll();
+    if(n>4){
+        n=1;
+    }else if(n<1){
+        n=4;
+    }
+    index=n;
+    document.getElementById("slide"+n).setAttribute("style","display:block;");
+}
+
+function hideAll(){
+    var i;
+    for(i=1;i<5;i++){
+        document.getElementById("slide"+i).setAttribute("style","display:none;");
+    }
+}
+
+setInterval(function(){ previewSlides(index+1); }, 5000);
+
+const navSlide=()=>{
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.navBar-links');
+    const navLinks = document.querySelectorAll('.navBar li');
+    burger.addEventListener('click',()=>{
+        nav.classList.toggle('navBar-active');
+        
+        navLinks.forEach((link, index)=>{
+            if(link.style.animation){
+                link.style.animation = '';
+            }else{
+                link.style.animation = 'navLinkFade 0.5s ease forwards '+(index/7+0.3)+'s';
+            }
+        });
+        burger.classList.toggle('toggle');
+    }); 
+}
